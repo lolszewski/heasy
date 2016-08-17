8f063f432119e98ff7970e8eb51c51422c71749a
Lukasz Olszewski <lsolszewski@gmail.com>
2016-08-17 20:33:25 +0200

    Initial commit

---
+++ src/main/java/com/oli/heasy/App.java
@@ -0,0 +1,11 @@
+package com.oli.heasy;
+
+/**
+ * @author Lukasz Olszewski <lsolszewski@gmail.com>
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
